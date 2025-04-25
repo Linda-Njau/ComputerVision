8c4b27f9594605bd93436cea1e399d8136432ffb
Linda Njau <lindanjau21@gmail.com>
2025-04-25 16:59:33 +0300

    [PyTorch] Add MNIST loading and convolution visualization functions
    Implemented load_mnist to download and prepare MNIST dataset with data loaders.
    Developed plot_convolution for visualizing convolutional effects on MNIST digits using custom filters.

---
+++ ConvolutionalNeuralNetworks/pytorchcv.py
@@ -0,0 +1,41 @@
+
+import builtins
+import torch
+import torch.nn as nn
+from torch.utils import data
+import torchvision
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import glob
+import os
+import zipfile
+
+default_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+def load_mnist(batch_size=64):
+    data_train = torchvision.datasets.MNIST('./data',download=True,train=True,transform=ToTensor())
+    data_test = torchvision.datasets.MNIST('./data', download=True,train=False,transform=ToTensor())
+    train_loader = torch.utils.data.DataLoader(data_train,batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(data_test,batch_size=batch_size)
+
+    return data_train,data_test,train_loader,test_loader
+
+def plot_convolution(t,title=''):
+    with torch.no_grad():
+        c = nn.Conv2d(kernel_size=(3,3),out_channels=1,in_channels=1)
+        c.weight.copy_(t)
+        fig, ax = plt.subplots(2,6,figsize=(8,3))
+        fig.suptitle(title,fontsize=16)
+        for i in range(5):
+            im = data_train[i][0]
+            ax[0][i].imshow(im[0])
+            ax[1][i].imshow(c(im.unsqueeze(0))[0][0])
+            ax[0][i].axis('off')
+            ax[1][i].axis('off')
+        ax[0,5].imshow(t)
+        ax[0,5].axis('off')
+        ax[1,5].axis('off')
+
+        plt.show()
