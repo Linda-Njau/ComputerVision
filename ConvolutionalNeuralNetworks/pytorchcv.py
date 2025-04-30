0c9a3559ae409cd3f81f4d01d1fe0a391206c24f
Linda-Njau <lindanjau21@gmail.com>
2025-04-30 17:15:04 +0300

    [PyTorch] Implement validate function for model evaluation
    Defines the validate function to evaluate the model's performance on the validation dataset.
    Switches the model to evaluation mode using net.eval() for inference.
    Calculates loss and accuracy without tracking gradients, and returns average values.

--- ConvolutionalNeuralNetworks/pytorchcv.py
+++ ConvolutionalNeuralNetworks/pytorchcv.py
@@ -41,6 +41,19 @@ def plot_convolution(t,data_train,title=''):
         #plt.tight_layout()
         plt.show()
 
+def validate(net, dataloader,loss_fn=nn.NLLLoss()):
+    net.eval()
+    count,acc,loss = 0,0,0
+    with torch.no_grad():
+        for features,labels in dataloader:
+            lbls = labels.to(default_device)
+            out = net(features.to(default_device))
+            loss += loss_fn(out,lbls)
+            pred = torch.max(out,1)[1]
+            acc += (pred==lbls).sum()
+            count += len(labels)
+        return loss.item()/count, acc.item()/count
+
 def train_epoch(net,dataloader, lr=0.01,optimizer=None,loss_fn = nn.LLLoss()):
     optimizer = optimizer or torch.optim.Adam(net.parameters(), lr=lr)
     net.train()
